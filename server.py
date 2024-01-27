dcb6da3a329dba16ceed08d6ec741413d8ff17f7
krizhnaa <krizhnaprem@gmail.com>
2024-01-27 11:36:27 +0530

    Generated a random number and finished with the homepage

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
+import random
 from flask import Flask
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Blah"
+    return '<h1>Guess a number between 0 and 9</h1>' \
+           '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">'
+
+gen_num = random.randint(0,9)
+print(gen_num)
 
 if __name__ == "__main__":
     app.run(debug=True)
