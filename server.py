60bb063671c4e5eb79f5ee4c5591b49ba4aff2a0
krizhnaa <krizhnaprem@gmail.com>
2024-01-27 11:45:51 +0530

    Done with the project

--- server.py
+++ server.py
@@ -1,16 +1,30 @@
 import random
 from flask import Flask
 
+gen_num = random.randint(0, 9)
+print(gen_num)
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def homepage():
     return '<h1>Guess a number between 0 and 9</h1>' \
            '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">'
 
-gen_num = random.randint(0,9)
-print(gen_num)
+
+@app.route("/<int:num>")
+def check_num(num):
+    if num < gen_num:
+        return '<h1>Number is Too Low</h1>' \
+               '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">'
+    elif num > gen_num:
+        return '<h1>Number is Too High</h1>' \
+               '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">'
+    elif num == gen_num:
+        return '<h1>You Found me</h1>' \
+               '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
